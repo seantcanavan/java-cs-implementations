cbf835ba51308732c82360ef7353d53682ffbb6b
seantcanavan@gmail.com <seantcanavan@gmail.com>
2017-11-28 14:44:38 -0800

    components + gradle + spring.

---
+++ src/main/java/com/seantcanavan/javacsimplementations/main/domain/components/SingleLinkNode.java
@@ -0,0 +1,32 @@
+package com.seantcanavan.javacsimplementations.main.domain.components;
+
+public class SingleLinkNode<T extends Comparable<T>> extends Node<T> {
+    private SingleLinkNode<T> one;
+
+    public SingleLinkNode(T data) { super(data); }
+
+    public SingleLinkNode<T> getOne() { return one; }
+
+    public void setOne(SingleLinkNode<T> one) { this.one = one; }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+
+        sb.append("| ");
+        if (data != null) {
+            sb.append(data.toString());
+        }
+        sb.append(" |");
+
+        if (one != null) {
+            sb.append(" => ");
+            sb.append("[");
+            sb.append(one.toString());
+            sb.append("]");
+        }
+
+        return sb.toString();
+    }
+}
