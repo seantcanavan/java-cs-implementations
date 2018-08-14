4b8c6c84c5b1cabd73bf3c2cbd62b0da4fdcd193
seantcanavan <seantcanavan@gmail.com>
2018-08-14 16:23:16 -0700

    finish arraylist initiailization and add

--- src/main/java/seantcanavan/SeanArrayList.java
+++ src/main/java/seantcanavan/SeanArrayList.java
@@ -1,72 +1,80 @@
 package seantcanavan;
 
-import java.io.Serializable;
 import java.util.Collection;
 
-public class SeanArrayList<E> implements Serializable {
-	private static final int DEFAULT_CAPACITY = 10;
-	private static final int GROWTH_RATE = 2;
-	private static final int SHRINK_RATE = 3;
-	private int size = -1;
-	private E[] list;
+public class SeanArrayList<T extends Comparable> {
+	public static final int DEFAULT_CAPACITY = 10;
+	public static final int GROWTH_RATE = 2;
+	public static final int SHRINK_RATE = 3;
+	private int size = 0;
+	private Object[] list = new Object[SeanArrayList.DEFAULT_CAPACITY];
+
+    public SeanArrayList() { }
 
 	public SeanArrayList(int initialSize) {
 		if (initialSize < 1) {
 			throw new IllegalArgumentException("Initial list size must be positive");
 		}
 
-		@SuppressWarnings("unchecked")
-		E[] newArray = (E[]) new Object[initialSize];
-		this.list = newArray;
+		this.list = new Object[initialSize];
 	}
 
-	public SeanArrayList() {
-		this(SeanArrayList.DEFAULT_CAPACITY);
-	}
-
-	public SeanArrayList(Collection<? extends E> collection) {
-		this(collection.size() + (collection.size() / 2));
+	public SeanArrayList(Collection<T> collection) {
 		this.addAll(collection);
 	}
 
-	public void addAll(Collection<? extends E> collection) {
-		for (E val : collection) {
+	public void addAll(Collection<T> collection) {
+		for (T val : collection) {
 			this.add(val);
 		}
 	}
 
-	public void add(E value) {
-		if (this.list.length == this.size) {
-			this.growCapacity(this.list.length + 1);
-		}
-
+	public void add(T value) {
+	    this.ensureCapacity();
 		this.list[size] = value;
 		this.size++;
 	}
+	
+	public void addAt(T value, int index) {
 
-	public void remove(E value) {
-		// the ratio of items in the list versus the capacity of the list
-		double ratio = this.list.length / this.size;
-		if (ratio < 0.67) {
-			this.shrinkCapacity();
-		}
+    }
 
-		this.size--;
-	}
-
-	public void growCapacity(int minSize) {
-
-		@SuppressWarnings("unchecked")
-		E[] newArray = (E[]) new Object[minSize * 2];
-
-		for(int x = 0; x < this.list.length; x++) {
-			newArray[x] = this.list[x];
-		}
-
-		this.list = newArray;
-	}
-
-	public void shrinkCapacity() {
+	public void remove(T value) {
 
 	}
+	
+	public void removeAt(int index) {
+		
+	}
+
+    @SuppressWarnings("unchecked")
+	public T get(int index) {
+        if (index > size) {
+            return null;
+        }
+
+        return (T) list[index];
+    }
+
+	private void ensureCapacity() {
+	    if (this.size < list.length) {
+	        return;
+        }
+
+        Object[] newArray = new Object[list.length * GROWTH_RATE];
+        System.arraycopy(list, 0, newArray, 0, list.length);
+        list = newArray;
+	}
+
+	private void shrinkCapacity() {
+
+	}
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getAllocatedCapacity() {
+	    return list.length;
+    }
 }
--- src/test/java/seantcanavan/SeanArrayListTest.java
+++ src/test/java/seantcanavan/SeanArrayListTest.java
@@ -1,10 +1,81 @@
 package seantcanavan;
 
+import org.junit.jupiter.api.Test;
+
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
 public class SeanArrayListTest {
-	public static void main(String[] args) {
-		
-		SeanArrayList<String> sal = new SeanArrayList<String>(20);
-		
-		System.out.println("test123");
-	}
+
+    private static class SeanElement implements Comparable<SeanElement> {
+
+        private int data;
+
+        public SeanElement() { }
+
+        public SeanElement(int data) { this.data = data; }
+
+        public int getData() {
+            return data;
+        }
+
+        public void setData(int data) {
+            this.data = data;
+        }
+
+        public static SeanElement buildRandom() {
+            return new SeanElement(new SecureRandom().nextInt(10000));
+        }
+
+        @Override
+        public int compareTo(SeanElement seanElement) {
+            return this.data - seanElement.getData();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            SeanElement that = (SeanElement) o;
+            return data == that.data;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(data);
+        }
+    }
+
+	@Test
+	public void shouldInitializeArrayList() {
+        // empty constructor
+        SeanArrayList<SeanElement> sal = new SeanArrayList<>();
+        assertThat(sal.getAllocatedCapacity()).isEqualTo(SeanArrayList.DEFAULT_CAPACITY);
+
+        // initial value constructor
+        int initialCapacity = 20;
+        sal = new SeanArrayList<>(initialCapacity);
+        assertThat(sal.getAllocatedCapacity()).isEqualTo(initialCapacity);
+
+        // initialize with another list
+        int initialElementCount = 27;
+        int reallocationCount = 27 / SeanArrayList.DEFAULT_CAPACITY;
+        List<SeanElement> initialElements = new ArrayList<>();
+        for (int i = 0; i < initialElementCount; i++) {
+            initialElements.add(SeanElement.buildRandom());
+        }
+
+        sal = new SeanArrayList<>(initialElements);
+        assertThat(sal.getSize()).isEqualTo(initialElementCount);
+        assertThat(sal.getAllocatedCapacity()).isEqualTo(SeanArrayList.DEFAULT_CAPACITY * 2 * reallocationCount);
+        assertThat(sal.getAllocatedCapacity()).isGreaterThan(sal.getSize());
+
+        for (int i = 0; i < initialElementCount; i++) {
+            assertThat(initialElements.get(i)).isEqualTo(sal.get(i));
+        }
+    }
 }
