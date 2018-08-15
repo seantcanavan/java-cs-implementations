0abe3191916dbe4a85fcd4288d8fcfd7dcd0dc7d
seantcanavan <seantcanavan@gmail.com>
2018-08-14 17:12:12 -0700

    finish add implementation

--- src/main/java/seantcanavan/SeanArrayList.java
+++ src/main/java/seantcanavan/SeanArrayList.java
@@ -3,78 +3,90 @@ package seantcanavan;
 import java.util.Collection;
 
 public class SeanArrayList<T extends Comparable> {
-	public static final int DEFAULT_CAPACITY = 10;
-	public static final int GROWTH_RATE = 2;
-	public static final int SHRINK_RATE = 3;
-	private int size = 0;
-	private Object[] list = new Object[SeanArrayList.DEFAULT_CAPACITY];
+  public static final int DEFAULT_CAPACITY = 10;
+  public static final int GROWTH_RATE = 2;
+  public static final int SHRINK_RATE = 3;
+  private int size = 0;
+  private Object[] list = new Object[SeanArrayList.DEFAULT_CAPACITY];
 
-    public SeanArrayList() { }
+  public SeanArrayList() {}
 
-	public SeanArrayList(int initialSize) {
-		if (initialSize < 1) {
-			throw new IllegalArgumentException("Initial list size must be positive");
-		}
-
-		this.list = new Object[initialSize];
-	}
-
-	public SeanArrayList(Collection<T> collection) {
-		this.addAll(collection);
-	}
-
-	public void addAll(Collection<T> collection) {
-		for (T val : collection) {
-			this.add(val);
-		}
-	}
-
-	public void add(T value) {
-	    this.ensureCapacity();
-		this.list[size] = value;
-		this.size++;
-	}
-	
-	public void addAt(T value, int index) {
-
+  public SeanArrayList(int initialSize) {
+    if (initialSize < 1) {
+      throw new IllegalArgumentException("Initial list size must be positive");
     }
 
-	public void remove(T value) {
+    this.list = new Object[initialSize];
+  }
 
-	}
-	
-	public void removeAt(int index) {
-		
-	}
+  public SeanArrayList(Collection<T> collection) {
+    this.addAll(collection);
+  }
 
-    @SuppressWarnings("unchecked")
-	public T get(int index) {
-        if (index > size) {
-            return null;
-        }
+  public void addAll(Collection<T> collection) {
+    for (T val : collection) {
+      this.add(val);
+    }
+  }
 
-        return (T) list[index];
+  public void add(T value) {
+    // always add to the end by default
+    this.add(value, size);
+  }
+
+  public void add(T value, int index) {
+
+    if (index < 0) {
+      throw new IllegalArgumentException("Can't add at negative index.");
     }
 
-	private void ensureCapacity() {
-	    if (this.size < list.length) {
-	        return;
-        }
-
-        Object[] newArray = new Object[list.length * GROWTH_RATE];
-        System.arraycopy(list, 0, newArray, 0, list.length);
-        list = newArray;
-	}
-
-	private void shrinkCapacity() {
-
-	}
-
-    public int getSize() {
-        return size;
+    if (index > size) {
+      index = size;
     }
 
-    public int getAllocatedCapacity() {
-	    return list.length;
+    // guarantee there's enough space to move everything to the right one space
+    this.ensureCapacity();
+
+    // start from the back and move every element to the right once
+    for (int i = size; i > index; i--) {
+      list[i] = list[i - 1];
     }
+
+    // insert the value in the new space
+    list[index] = value;
+    this.size++;
+  }
+
+  public void remove(T value) {}
+
+  public void removeAt(int index) {}
+
+  @SuppressWarnings("unchecked")
+  public T get(int index) {
+    if (index > size) {
+      return null;
+    }
+
+    return (T) list[index];
+  }
+
+  private void ensureCapacity() {
+    if (this.size < list.length) {
+      return;
+    }
+
+    Object[] newArray = new Object[list.length * GROWTH_RATE];
+    System.arraycopy(list, 0, newArray, 0, list.length);
+    list = newArray;
+  }
+
+  private void shrinkCapacity() {}
+
+  public int getSize() {
+    return size;
+  }
+
+  public int getAllocatedCapacity() {
+    return list.length;
+  }
 }
--- src/test/java/seantcanavan/SeanArrayListTest.java
+++ src/test/java/seantcanavan/SeanArrayListTest.java
@@ -11,71 +11,161 @@ import static org.fest.assertions.api.Assertions.assertThat;
 
 public class SeanArrayListTest {
 
-    private static class SeanElement implements Comparable<SeanElement> {
+  private static class DataElement implements Comparable<DataElement> {
 
-        private int data;
+    private int data;
 
-        public SeanElement() { }
+    public DataElement() {}
 
-        public SeanElement(int data) { this.data = data; }
-
-        public int getData() {
-            return data;
-        }
-
-        public void setData(int data) {
-            this.data = data;
-        }
-
-        public static SeanElement buildRandom() {
-            return new SeanElement(new SecureRandom().nextInt(10000));
-        }
-
-        @Override
-        public int compareTo(SeanElement seanElement) {
-            return this.data - seanElement.getData();
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            SeanElement that = (SeanElement) o;
-            return data == that.data;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(data);
-        }
+    public DataElement(int data) {
+      this.data = data;
     }
 
-	@Test
-	public void shouldInitializeArrayList() {
-        // empty constructor
-        SeanArrayList<SeanElement> sal = new SeanArrayList<>();
-        assertThat(sal.getAllocatedCapacity()).isEqualTo(SeanArrayList.DEFAULT_CAPACITY);
-
-        // initial value constructor
-        int initialCapacity = 20;
-        sal = new SeanArrayList<>(initialCapacity);
-        assertThat(sal.getAllocatedCapacity()).isEqualTo(initialCapacity);
-
-        // initialize with another list
-        int initialElementCount = 27;
-        int reallocationCount = 27 / SeanArrayList.DEFAULT_CAPACITY;
-        List<SeanElement> initialElements = new ArrayList<>();
-        for (int i = 0; i < initialElementCount; i++) {
-            initialElements.add(SeanElement.buildRandom());
-        }
-
-        sal = new SeanArrayList<>(initialElements);
-        assertThat(sal.getSize()).isEqualTo(initialElementCount);
-        assertThat(sal.getAllocatedCapacity()).isEqualTo(SeanArrayList.DEFAULT_CAPACITY * 2 * reallocationCount);
-        assertThat(sal.getAllocatedCapacity()).isGreaterThan(sal.getSize());
-
-        for (int i = 0; i < initialElementCount; i++) {
-            assertThat(initialElements.get(i)).isEqualTo(sal.get(i));
-        }
+    public int getData() {
+      return data;
     }
+
+    public void setData(int data) {
+      this.data = data;
+    }
+
+    public static DataElement getRandom() {
+      return new DataElement(new SecureRandom().nextInt(10000));
+    }
+
+    @Override
+    public int compareTo(DataElement dataElement) {
+      return this.data - dataElement.getData();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      DataElement that = (DataElement) o;
+      return data == that.data;
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(data);
+    }
+  }
+
+  @Test
+  public void Constructor_WithAllConstructorArguments_ShouldCorrectlyInitializeSeanArrayList() {
+    // empty constructor
+    SeanArrayList<DataElement> sal = new SeanArrayList<>();
+    assertThat(sal.getAllocatedCapacity()).isEqualTo(SeanArrayList.DEFAULT_CAPACITY);
+
+    // initial value constructor
+    int initialCapacity = 20;
+    sal = new SeanArrayList<>(initialCapacity);
+    assertThat(sal.getAllocatedCapacity()).isEqualTo(initialCapacity);
+
+    // initialize with another list
+    int initialElementCount = 27;
+    int reallocationCount = 2;
+    List<DataElement> initialElements = new ArrayList<>();
+    for (int i = 0; i < initialElementCount; i++) {
+      initialElements.add(DataElement.getRandom());
+    }
+
+    sal = new SeanArrayList<>(initialElements);
+    assertThat(sal.getSize()).isEqualTo(initialElementCount);
+    assertThat(sal.getAllocatedCapacity())
+        .isEqualTo(SeanArrayList.DEFAULT_CAPACITY * 2 * reallocationCount);
+    assertThat(sal.getAllocatedCapacity()).isGreaterThan(sal.getSize());
+
+    for (int i = 0; i < initialElementCount; i++) {
+      assertThat(initialElements.get(i)).isEqualTo(sal.get(i));
+    }
+  }
+
+  @Test
+  public void Add_WithMultipleValuesInARow_ShouldGrowAndAddInCorrectOrder() {
+    int toAddCount = 80;
+    List<DataElement> elementsToAdd = getRandomElements(toAddCount);
+    SeanArrayList<DataElement> sal = new SeanArrayList<>();
+
+    for (DataElement currentElement : elementsToAdd) {
+      sal.add(currentElement);
+    }
+
+    assertThat(sal.getAllocatedCapacity()).isEqualTo(80);
+    assertThat(sal.getSize()).isEqualTo(80);
+
+    for (int i = 0; i < elementsToAdd.size(); i++) {
+      assertThat(sal.get(i)).isEqualTo(elementsToAdd.get(i));
+    }
+  }
+
+  @Test
+  public void Add_WithMultipleValuesInTheSameIndex_ShouldGrowAndPushValuesRight() {
+    int toAddCount = 81;
+    int offset = 6;
+    List<DataElement> elementsToAdd = getRandomElements(toAddCount);
+    SeanArrayList<DataElement> sal = new SeanArrayList<>();
+
+    for (DataElement currentElement : elementsToAdd) {
+      sal.add(currentElement, offset);
+    }
+
+    assertThat(sal.getSize()).isEqualTo(toAddCount);
+    assertThat(sal.getAllocatedCapacity()).isEqualTo(160);
+
+    for (int i = 0; i < offset; i++) {
+      assertThat(elementsToAdd.get(i)).isEqualTo(sal.get(i));
+    }
+
+    int reverseCount = 1;
+    for (int i = offset; i < elementsToAdd.size(); i++) {
+      assertThat(sal.get(i)).isEqualTo(elementsToAdd.get(elementsToAdd.size() - reverseCount));
+      reverseCount++;
+    }
+  }
+
+  @Test
+  public void Add_AlwaysAtTheEnd_ShouldNotOverGrowList() {
+    int toAddCount = 10;
+    List<DataElement> elementsToAdd = getRandomElements(toAddCount);
+    SeanArrayList<DataElement> sal = new SeanArrayList<>();
+
+    for (int i = 0; i < elementsToAdd.size(); i++) {
+      sal.add(elementsToAdd.get(i), i);
+    }
+
+    assertThat(sal.getAllocatedCapacity()).isEqualTo(toAddCount);
+    assertThat(sal.getSize()).isEqualTo(toAddCount);
+  }
+
+  @Test
+  public void Add_AlwaysAtTheBeginning_ShouldReverseInputList() {
+    int toAddCount = 100;
+    List<DataElement> elementsToAdd = getRandomElements(toAddCount);
+    SeanArrayList<DataElement> sal = new SeanArrayList<>();
+
+    for (DataElement currentElement : elementsToAdd) {
+      sal.add(currentElement, 0);
+    }
+
+    int reverseCount = 1;
+    for (int i = 0; i < elementsToAdd.size(); i++) {
+      assertThat(sal.get(i)).isEqualTo(elementsToAdd.get(elementsToAdd.size() - reverseCount));
+      reverseCount++;
+    }
+
+    // triple check that the first value and last values have swapped
+    assertThat(sal.get(0)).isEqualTo(elementsToAdd.get(toAddCount - 1));
+    assertThat(sal.get(toAddCount - 1)).isEqualTo(elementsToAdd.get(0));
+  }
+
+  private List<DataElement> getRandomElements(int length) {
+    List<DataElement> elements = new ArrayList<>();
+    for (int i = 0; i < length; i++) {
+      elements.add(DataElement.getRandom());
+    }
+
+    return elements;
+  }
 }
